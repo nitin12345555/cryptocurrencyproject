8841afef82b70090d67aa274bcfaa2c653ce0878
Nitin Jangid <nitinjangidj92@gmail.com>
2025-07-29 10:05:29 -0700

    Update Auth.js

--- backend/Middlewares/Auth.js
+++ backend/Middlewares/Auth.js
@@ -15,7 +15,7 @@ const ensureAuthenticated = (req, res, next) => {
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = decoded; // Attach user info to the request object
+    req.user = decoded;
     next();
   } catch (err) {
     return res.status(403).json({ message: "Unauthorized, JWT token wrong or expired" });
