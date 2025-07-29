db43e25cfe848c7b617af22eb5ed8adfc8b1e51e
Nitin Jangid <nitinjangidj92@gmail.com>
2025-07-29 10:06:28 -0700

    Update db.js

--- backend/Models/db.js
+++ backend/Models/db.js
@@ -1,8 +1,7 @@
-// Models/db.js
 const mongoose = require('mongoose');
 
-const mongo_url = process.env.MONGO_CONN; // Access the connection string from environment variables
+const mongo_url = process.env.MONGO_CONN;
 
 mongoose.connect(mongo_url)
-    .then(() => console.log('Mongo DB connected')) // Log success
-    .catch((err) => console.log('MongoDB Connection error:', err)); // Log specific error
+    .then(() => console.log('Mongo DB connected'))
+    .catch((err) => console.log('MongoDB Connection error:', err));
